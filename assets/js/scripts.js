e0ee0e4b4ea36b77a2730fe31a8cbb1c11f6a5f7
Payam Meyer <payam.meyer@ascend-tech.us>
2017-08-01 13:12:30 -0400

    Updated with the construction page.

---
+++ assets/js/scripts.js
@@ -0,0 +1,84 @@
+/*
+ Author: Ukieweb
+ Template: Rock (Coming Soon)
+ Version: 1.s
+ URL: http://themeforest.net/user/UkieWeb
+ */
+
+
+$(document).ready(function(){
+
+    "use strict";
+
+
+    /*
+     ----------------------------------------------------------------------
+     Preloader
+     ----------------------------------------------------------------------
+     */
+    $(".loader").delay(400).fadeOut();
+    $(".animationload").delay(400).fadeOut("fast");
+
+
+    /*
+     ----------------------------------------------------------------------
+     Nice scroll
+     ----------------------------------------------------------------------
+     */
+    $("html").niceScroll({
+        cursorcolor: '#fff',
+        cursoropacitymin: '0',
+        cursoropacitymax: '1',
+        cursorwidth: '2px',
+        zindex: 999999,
+        horizrailenabled: false,
+        enablekeyboard: false
+    });
+
+
+    /*
+     ----------------------------------------------------------------------
+     Watch
+     ----------------------------------------------------------------------
+     */
+    if($.find('#watch')[0]) {
+
+        $('#watch').countDown({
+            targetDate: {
+                'day': 		10,
+                'month': 	9,
+                'year': 	2017,
+                'hour': 	11,
+                'min': 		13,
+                'sec': 		0
+            },
+            omitWeeks: true
+        });
+        //enter the count down date using the format year, month, day, time: hour, min, sec
+
+        if( $('.day_field .top').html() == "0" ) $('.day_field').css('display','none');
+
+    }
+
+}); // End $(document).ready(function(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
